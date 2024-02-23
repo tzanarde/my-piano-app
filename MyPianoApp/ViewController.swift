1a4d2df1152ed57576dddaecf4d5f8ab1cb872b5
Tiago Zanarde <zanarde.tiago@gmail.com>
2024-02-23 11:29:16 -0300

    first commit - v0.1

--- MyPianoApp/ViewController.swift
+++ MyPianoApp/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer!
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(key: sender.titleLabel?.text ?? "")
     }
-
-
+    
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
 }
 
